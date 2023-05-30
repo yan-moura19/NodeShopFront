f7d198f1af28cbdcd7931f86bbafb8487e9112bf
yan-moura19 <yangabriel3301@gmail.com>
2023-05-30 18:42:04 -0300

    DockerFile e dockercompose

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Yan\Desktop\Projetos\NodeShopFront\nodeshop_front"
+export "FLUTTER_APPLICATION_PATH=G:\area de trabalho\Projetos\NodeShopFront\teste\NodeShopFront"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
